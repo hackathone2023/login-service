73f6f171c0db9c6de095e1bbb2326ff1166bd325
Mohammed Khazi <mohkhazi@publicisgroupe.net>
2023-03-26 00:51:21 +0530

    login controller

--- src/main/java/com/solutions/evergreen/loginservice/LoginServiceApplication.java
+++ src/main/java/com/solutions/evergreen/loginservice/LoginServiceApplication.java
@@ -2,11 +2,17 @@ package com.solutions.evergreen.loginservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaClient
+@RestController
 public class LoginServiceApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "Congrats ! your application deployed successfully in Azure Platform. !";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginServiceApplication.class, args);
